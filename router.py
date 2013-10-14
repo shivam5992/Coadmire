cf546b60c0cbc4b6351e14da280245e156bd4f98
Shivam Bansal <shivam5992@gmail.com>
2013-10-14 12:30:31 -0700

    resonse check added

--- router.py
+++ router.py
@@ -4,15 +4,23 @@ from pymongo import MongoClient
 
 client = MongoClient()
 db = client.coadtest
+
 collection = db.level1
 
 app = Flask(__name__)
 app.secret_key = 'shivam_bansal'
 
-@app.route('/')
-def index():
-	return render_template('index.html',collection =  collection)
 
+@app.route('/',methods=['GET','POST'])
+def index(): 
+	error = None
+	if request.method == 'POST':
+		if request.form['ques'] == '21':
+			error = 'you response is correct'
+		else:
+			error = 'you response is not correct'
+	return render_template('index.html',error=error,collection =  collection)
+	
 if __name__ == '__main__' :
 	app.run(debug=True)
 
