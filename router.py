a6250d7b4f3875a254fa9688ac1d483c4403872e
Shivam Bansal <shivam5992@gmail.com>
2013-10-14 15:03:37 -0700

    randomness in questions added

--- router.py
+++ router.py
@@ -1,25 +1,35 @@
 from flask import *
 from functools import wraps
 from pymongo import MongoClient
+import random
 
 client = MongoClient()
 db = client.coadtest
-
-collection = db.level1
+level_id = 1
 
 app = Flask(__name__)
 app.secret_key = 'shivam_bansal'
 
-
 @app.route('/',methods=['GET','POST'])
 def index(): 
-	error = None
+	resp = None
+
+	level_id = 1
+
+	#level = "level" + str(level_id)
+	collection = db.level1
+	
+	Ques_id = random.randrange(1,6)
+	doc = collection.find_one({ '_id' : Ques_id })
+
 	if request.method == 'POST':
-		if request.form['ques'] == '21':
-			error = 'you response is correct'
+		if request.form['ques'] == doc['answer']:
+			resp = 'you response is correct'
+			level_id += 1
 		else:
-			error = 'you response is not correct'
-	return render_template('index.html',error=error,collection =  collection)
+			resp = 'you response is not correct'
+			# level_id -= 1
+	return render_template('index.html',resp = resp,doc =  doc)
 	
 if __name__ == '__main__' :
 	app.run(debug=True)
