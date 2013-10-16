19ecaf98bca70c863c5429571d31ea95b4302258
Shivam Bansal <shivam5992@gmail.com>
2013-10-16 00:37:34 -0700

    backend completed

--- insert.py
+++ insert.py
@@ -179,3 +179,62 @@ collection3.insert(q32)
 collection3.insert(q33)
 collection3.insert(q34)
 collection3.insert(q35)
+
+collection4 = db.level4 
+
+collection4.drop()
+
+q41 ={
+	'_id' 		: 	1,
+	'question' 		: 	'4 What is your age ?',
+	'optiona' 	: 	'21',
+	'optionb' 	: 	'22',
+	'optionc' 	: 	'23',
+	'optiond' 	: 	'24',
+	'answer'	: 	'21',
+}
+q42 ={
+	'_id' 		: 	2,
+	'question' 		: 	'4 What is your name ?',
+	'optiona' 	: 	'af',
+	'optionb' 	: 	'bd',
+	'optionc' 	: 	'ca',
+	'optiond' 	: 	'dq',
+	'answer'	: 	'bd',
+}
+
+q43 ={
+	'_id' 		: 	3,
+	'question' 		: 	'4 What is your class ?',
+	'optiona' 	: 	'12',
+	'optionb' 	: 	'11',
+	'optionc' 	: 	'10',
+	'optiond' 	: 	'9',
+	'answer'	: 	'11',
+}
+
+q44 ={
+	'_id' 		: 	4,
+	'question' 		: 	'4 What is your profession ?',
+	'optiona' 	: 	'doc',
+	'optionb' 	: 	'paint',
+	'optionc' 	: 	'eng',
+	'optiond' 	: 	'web',
+	'answer'	: 	'doc',
+}
+
+q45 ={
+	'_id' 		: 	5,
+	'question' 		: 	'4 What is your color ?',
+	'optiona' 	: 	'red',
+	'optionb' 	: 	'green',
+	'optionc' 	: 	'blue',
+	'optiond' 	: 	'pink',
+	'answer'	: 	'red',
+}
+
+collection4.insert(q41)
+collection4.insert(q42)
+collection4.insert(q43)
+collection4.insert(q44)
+collection4.insert(q45)
--- router.py
+++ router.py
@@ -7,7 +7,6 @@ client = MongoClient()
 db = client.coadtest
 
 level_id = 1
-flag = "same"
 count = 0
 
 app = Flask(__name__)
@@ -16,39 +15,24 @@ app.secret_key = 'shivam_bansal'
 @app.route('/',methods=['GET','POST'])
 def index(): 
 	resp = None
-	global flag
 	global level_id
-	global count 
-	level = "level1"
 
-	if flag == "increased":
-		level = "level" + str(level_id-1)
-	elif flag == "decreased":
-		level = "level" + str(level_id+1)
-	else:
-		level = "level" + str(level_id)	
-	
 	# previous question check
 	if request.method == 'POST':
+		level = "level" + str(level_id)
 		Q = request.form['question']
 		pcollection = db[level]
 		pdoc = pcollection.find_one({ 'question' : Q })
-		flash(pdoc['answer'])
-
-		if request.form['ques_response'] == pdoc['answer']:
+		A = pdoc['answer']
+		if request.form['ques_response'] == A:
 			resp = 'you response was correct'
-			if level_id < 3:
+			if level_id != 4:
 				level_id += 1
-				flag = "increased"
 		else:
 			resp = 'you response was not correct'
-			if level_id > 1:
+			if level_id != 1:
 				level_id -= 1
-				flag = "decreased"
 
-	count += 1
-	flash(count)
-	# New level accn to new levelid,random ques from that level and displayed on screen.
 	level = "level" + str(level_id)
 	collection = db[level]
 	Ques_id = random.randrange(1,5)
@@ -57,3 +41,4 @@ def index():
 	
 if __name__ == '__main__' :
 	app.run(debug=True)
+
