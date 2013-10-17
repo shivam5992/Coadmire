9653a0b0790c34fb1863d3e0858cae76b125298e
Shivam Bansal <shivam5992@gmail.com>
2013-10-16 23:43:49 -0700

    results stored for particular user

--- router.py
+++ router.py
@@ -6,12 +6,15 @@ import random
 client = MongoClient()
 db = client.coadtest
 
-level_id = 0
-count = 0
+#set these variables
 max_questions = 30
-u_res = 0
 positive_marks_for_question = 4
 negative_marks_for_question = 1
+
+level_id = 0
+count = 0
+user_score_so_far = 0
+
 
 app = Flask(__name__)
 app.secret_key = 'shivam_bansal'
@@ -19,14 +22,13 @@ app.secret_key = 'shivam_bansal'
 @app.route('/',methods=['GET','POST'])
 def index(): 
 	resp = None
+	user_name = "shivam"
+	user_id =  "10103475"
 	global level_id
 	global count 
-	global max_questions
-	global u_res
+	global user_score_so_far
 
-	# previous question check
 	if request.method == 'POST':
-		#question check
 		level = "level" + str(level_id)
 		Q = request.form['question']
 		pcollection = db[level]
@@ -36,37 +38,36 @@ def index():
 			resp = 'you response was correct'
 			if level_id != 9:
 				level_id += 1
-			u_res += positive_marks_for_question
+			user_score_so_far += positive_marks_for_question
 		else:
 			resp = 'you response was not correct'
 			if level_id != 0:
 				level_id -= 1
-			u_res -= negative_marks_for_question
+			user_score_so_far -= negative_marks_for_question
 
-		#store result
+		
+		# store result for a particular user, store each question as docs
+		userResponse = "ReponseOf"+user_name+user_id
+		
 		user_level = level
 		user_question = request.form['question']
-		user_response = request.form['ques_response']
 		user_answer = pdoc['answer']
-		user_result = u_res
-		user_time_spent = 0
-		#change collection name for particular user
-		collection = db.userResponse
+		user_response = request.form['ques_response']
+		user_result = user_score_so_far
+		
+		collection = db[userResponse]
 		result_doc ={
+		'userName' : user_name,
+		'UserId'  : user_id,
+		'level':	 user_level,
 		'question' : user_question,
 		'answer' :   user_answer,
 		'response' : user_response,
-		'time':      user_time_spent,
-		'level':	 user_level
+		'score'  :   user_result
 		}
 		collection.insert(result_doc)
 		
-		'''flash(user_level)
-		flash(user_question)
-		flash(user_response)
-		flash(user_result)
-		flash(user_time_spent)
-		flash(user_answer)'''
+		
 	
 	count += 1
 	if count == max_questions+1:
@@ -78,10 +79,7 @@ def index():
 		Ques_id = random.randrange(1,5)
 		doc = collection.find_one({ '_id' : Ques_id })
 		return render_template('index.html',doc =  doc)
-
-@app.route('/box')
-def box():
-	return render_template('box.html') 
+ 
 	
 if __name__ == '__main__' :
 	app.run(debug=True)
